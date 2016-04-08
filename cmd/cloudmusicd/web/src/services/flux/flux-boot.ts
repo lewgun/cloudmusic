5d615e0a161a8f7d6a5fe7bdbf69ef1b037f84da
lewgun <lewgun@gmail.com>
2016-04-08 18:03:14 +0800

    add missing file

--- cmd/cloudmusicd/web/src/services/flux/flux-boot.ts
+++ cmd/cloudmusicd/web/src/services/flux/flux-boot.ts
@@ -5,10 +5,12 @@ import {Dispatcher}  from "./dispatcher/dispatcher";
 
 import {UserInfoActionCreator}  from "./actions/userinfo-action";
 import {PlayListActionCreator}  from "./actions/playlist-action";
+import {PlayActionCreator}  from "./actions/play-action";
+
 
 import {UserInfoStore } from "./stores/userinfo-store";
 import {PlayListStore } from "./stores/playlist-store";
-
+import {PlayStore } from "./stores/play-store";
 
 @Injectable()
 export class FluxBoot {
@@ -19,7 +21,10 @@ export class FluxBoot {
         @Inject(UserInfoActionCreator) private _uiac: UserInfoActionCreator,
 
         @Inject(PlayListStore) private _pls: PlayListStore,
-        @Inject(PlayListActionCreator) private _plac: PlayListActionCreator
+        @Inject(PlayActionCreator) private _pac: PlayActionCreator,
+
+        @Inject(PlayStore) private _ps: PlayStore,
+        @Inject(PlayActionCreator) private _plac: PlayListActionCreator
 
     ) { }
 }
