93b87cae6e5d29981690d61d90fb5d3b78930550
lewgun <lewgun@gmail.com>
2016-04-14 17:55:10 +0800

    copy code from angular2 material

---
+++ cmd/cloudmusicd/web/src/core/key-codes.ts
@@ -0,0 +1,17 @@
+import {CONST} from "angular2/src/facade/lang";
+
+@CONST()
+export class KeyCodes {
+    
+    @CONST()
+    static ESACPE = 27;
+    
+    @CONST()
+    static SPACE = 32;
+    
+    @CONST()
+    static UP = 38;
+    
+    @CONST()
+    static DOWN = 40;
+}
